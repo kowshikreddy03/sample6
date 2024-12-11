e49b17df0f94221ab29002607de9dac976e82586
kowshik <kowshikvardhanreddy@gmail.com>
2024-12-11 11:04:20 +0530

    Third commit

--- one_to_hundred.java
+++ one_to_hundred.java
@@ -1,6 +1,6 @@
 public class one_to_hundred {
     public static void main(String[] args) {
-        System.out.println("Prime numbers are:");
+        System.out.println("Prime numbers:");
         for (int i = 2; i <= 100; i++) {
             if (isPrime(i)) {
                 System.out.print(i + " ");
